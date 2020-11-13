32c65309a745c9d13432fb3aa0128e99359f66dd
Chelsea Dabre <chelseadabre@gmail.com>
2020-11-13 18:16:35 +0530

    BN

---
+++ Controllers/User.js
@@ -0,0 +1,17 @@
+
+/*const User = require('../Models/User');
+
+exports.signUp = (req,res,next) =>{
+        const email = req.body.email;
+        const password = req.body.password;
+        const firstname = req.body.firstname;
+        const lastname = req.body.lastname;
+        const SignInUser = new User(
+ )
+        city.find().then(response =>{
+                res.status(200).json({message:"Location fetched Successfully",location:response})
+        })
+        .catch(err=> {
+                res.status(500).json({ message:err})
+        })
+}*/
